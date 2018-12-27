9f267df451b2dde0033e01a56415a30356f59dce
zhangkun <zhangkun09211916@gmail.com>
2018-12-27 18:13:00 +0800

    fluent python

---
+++ Learning_manual/codetest/fluent_python/fulent_python2_9.py
@@ -0,0 +1,26 @@
+
+from collections import namedtuple
+
+City = namedtuple('City', 'name country population coordinates')
+
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+
+print(tokyo)
+
+print(tokyo.population)
+
+print(tokyo[1])
+
+print('---------------------')
+
+print(City._fields)
+
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)
+print(delhi._asdict())
+
+print('---------------------')
+
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
