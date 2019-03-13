82ec37c869aadcc150287086103a947912800fce
zhangkun <zhangkun09211916@gmail.com>
2019-03-13 17:28:18 +0800

    Interview question

--- test001.py
+++ test001.py
@@ -1,18 +1,47 @@
-from time import ctime, sleep
+import unittest
+
+from mydict import  Dict
+
+class TestDict(unittest.TestCase):
+
+    def setUp(self):
+        print('setUp...')
+
+    def tearDown(self):
+        print('tear Down..')
 
 
-def timefunc_age(pre="hello"):
-    def timefunc(func):
-        def wrappedfunc():
-            print("%s called at %s %s" % (func.__name__, ctime(), pre))
-            return func()
-        return  wrappedfunc
-    return timefunc
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key,'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
 
 
-@timefunc_age('world')
-def foo():
-    print('I am foo')
 
-foo()
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        unittest.main()
+
 
